02feb8241597c7451d00b0176f51b38896eaf5f6
DanMadar27 <danmadar27@gmail.com>
2023-07-26 14:32:49 +0300

    Add filled header to register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,7 +5,11 @@ import classes from './register.module.css';
 export default function Home() {
   return (
     <div className={classes.container}>
-      <h1>Register</h1>
+      <div className='filled'>
+        <h1>Shop</h1>
+      </div>
+      <h2>Welcome to Shop!!</h2>
+      <h3>Register to Continue</h3>
       <RegisterForm />
     </div>
   )
