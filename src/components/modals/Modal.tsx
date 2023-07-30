df1eac8f1189a423599677824e72854abe291f8d
DanMadar27 <danmadar27@gmail.com>
2023-07-30 15:19:47 +0300

    Allow user to exit modal using outside click or escape key

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import styles from './modal.module.css';
 
@@ -13,11 +13,36 @@ const Modal = (props: Props) => {
   
   if (!isOpen) return null;
 
-  const style = isOpen ? styles.open : styles.closed;
+  const modalContentRef = useRef<HTMLDivElement>(null);
+
+  // If the user clicks outside of the modal or presses the escape key, close the modal
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (modalContentRef.current && !modalContentRef.current.contains(event.target as Node)) {
+        onClose();
+      }
+    };
+
+    const handleEscapeKey = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('mousedown', handleOutsideClick);
+      document.addEventListener('keydown', handleEscapeKey);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+      document.removeEventListener('keydown', handleEscapeKey);
+    };
+  }, [isOpen, onClose]);
 
   return (
     <div className={styles.modal}>
-      <div className={`${styles.content}`}>
+      <div className={styles.content} ref={modalContentRef}>
         {children}
         <button onClick={onClose}>
           Close
