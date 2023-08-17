4ca3992e04dcb158781fcc70577645b3bbecf90d
DanMadar27 <danmadar27@gmail.com>
2023-08-17 16:55:18 +0300

    Revert "Use a tag for links in navbar"
    This reverts commit aeecf5d9cd747e37b8ea8571025b86b00ba7f99d.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import { usePathname } from 'next/navigation'
+import Link from 'next/link';
 
 import styles from './navbar.module.css';
 import 'material-icons/iconfont/filled.css';
@@ -25,22 +26,22 @@ const Navbar: React.FC = () => {
         />
       </div>
       <div className={`${styles.links} ${showLinks ? styles['show-links'] : ''}`}>
-        <a href='/' className={pathname === '/' ? styles.active : ''}>
+        <Link href='/' className={pathname === '/' ? styles.active : ''}>
           <span className='material-icons'>home</span>
           Home
-        </a>
-        <a href='/products' className={pathname.startsWith('/products') ? styles.active : ''}>
+        </Link>
+        <Link href='/products' className={pathname.startsWith('/products') ? styles.active : ''}>
           <span className='material-icons'>inventory_2</span>
           Products
-        </a>
-        <a href='/cart' className={pathname === '/cart' ? styles.active : ''}>
+        </Link>
+        <Link href='/cart' className={pathname === '/cart' ? styles.active : ''}>
           <span className='material-icons'>shopping_cart</span>
           Cart
-        </a>
-        <a href='/login' className={pathname === '/login' ? styles.active : ''}>
+        </Link>
+        <Link href='/login' className={pathname === '/login' ? styles.active : ''}>
           <span className='material-icons'>login</span>
           Login
-        </a>
+        </Link>
       </div>
       <div 
         className={`${styles.hamburger} ${showLinks ? styles.open : ''}`}
