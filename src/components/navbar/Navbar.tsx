790122861c1291059f39cb9603a52cf63923b9a5
DanMadar27 <danmadar27@gmail.com>
2023-08-17 15:55:08 +0300

    Add icons to links in navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,9 @@
 import React, { useState } from 'react';
 import { usePathname } from 'next/navigation'
 import Link from 'next/link';
+
 import styles from './navbar.module.css';
+import 'material-icons/iconfont/filled.css';
 
 const Navbar: React.FC = () => {
   const pathname = usePathname();
@@ -25,15 +27,19 @@ const Navbar: React.FC = () => {
       </div>
       <div className={`${styles.links} ${showLinks ? styles['show-links'] : ''}`}>
         <Link href='/' className={pathname === '/' ? styles.active : ''}>
+          <span className='material-icons'>home</span>
           Home
         </Link>
         <Link href='/products' className={pathname.startsWith('/products') ? styles.active : ''}>
+          <span className='material-icons'>inventory_2</span>
           Products
         </Link>
         <Link href='/cart' className={pathname === '/cart' ? styles.active : ''}>
+          <span className='material-icons'>shopping_cart</span>
           Cart
         </Link>
         <Link href='/login' className={pathname === '/login' ? styles.active : ''}>
+          <span className='material-icons'>login</span>
           Login
         </Link>
       </div>
