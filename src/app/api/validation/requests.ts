496c5671923f8d54807b5e8e092f3df14dc186c2
DanMadar27 <danmadar27@gmail.com>
2023-09-18 10:13:59 +0300

    Validate all parameters in middleware

--- src/app/api/validation/requests.ts
+++ src/app/api/validation/requests.ts
@@ -1,4 +1,38 @@
+import { NextResponse, type NextRequest } from 'next/server';
 import { validateSearch } from '@/utils/validation';
+
+async function getParams(request: NextRequest) {
+  switch (request.method) {
+    case 'GET':
+      return new URL(request.url).searchParams.toString();
+    case 'POST':
+    case 'PUT':
+    case 'PATCH':
+      return await request.json();
+    default:
+      return null;
+  }
+}
+
+async function isMaliciousRequest(request: NextRequest) {
+  const params = JSON.stringify(await getParams(request));
+  const sqlInjectionPattern = /SELECT|INSERT|UPDATE|DELETE|DROP|UNION|OR\s+1\s*=\s*1/i;
+
+  return sqlInjectionPattern.test(params);
+}
+
+export async function validateRequest(request: NextRequest) {
+  if (await isMaliciousRequest(request)) {
+    console.error('Malicious request');
+
+    return NextResponse.json(
+      { error: 'Bad request' }, { status: 400 }
+    );
+  }
+
+  // Can add rate limiting here
+  return NextResponse.next();
+}
 
 export function validateSlug(slug: string) {
   return !isNaN(parseInt(slug));
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import type { NextRequest } from 'next/server';
 import { logRequest } from './utils/logs';
+import { validateRequest } from './app/api/validation/requests';
 
 export async function middleware(request: NextRequest) {
   await logRequest(request);
+  return await validateRequest(request);
 }
