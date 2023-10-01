9f0c3e143c2d80cc451652975dde8baf16405e70
DanMadar27 <danmadar27@gmail.com>
2023-10-02 02:55:42 +0300

    Initialize data in db if not set

--- src/app/api/db/client.ts
+++ src/app/api/db/client.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from '@prisma/client';
+import { initData } from './data/utils';
 
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
+  initData(prisma);
 }
 else {
   let globalWithPrisma = global as typeof globalThis & {
@@ -12,7 +14,9 @@ else {
   
   if (!globalWithPrisma.prisma) {
     globalWithPrisma.prisma = new PrismaClient();
+    initData(globalWithPrisma.prisma);
   }
+  
   prisma = globalWithPrisma.prisma;
 }
 
---
+++ src/app/api/db/data/utils.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from '@prisma/client';
+import initProducts from './products';
+
+export async function initData(prisma: PrismaClient) {
+  if (!await isInitData(prisma)) {
+    await createInitData(prisma);
+  }
+}
+
+async function isInitData(prisma: PrismaClient) {
+  if (!await productsExist(prisma)) {
+    return false;
+  }
+
+  return true;
+}
+
+async function createInitData(prisma: PrismaClient) {
+  console.log('Creating initial data');
+
+  await Promise.all([
+    createProducts(prisma),
+  ]);
+
+  console.log('Initial data created');
+}
+
+async function productsExist(prisma: PrismaClient) {
+  const products = await prisma.product.findMany();
+  return products.length > 0;
+}
+
+async function createProducts(prisma: PrismaClient) {
+  await prisma.product.createMany({
+    data: initProducts,
+  });
+
+  console.log('Products created');
+}
